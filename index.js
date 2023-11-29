ff65702c13d999b618bb8dca577bda983e9772f6
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-11-30 00:19:53 +0100

    sucessfully aggregated menuids data

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// console.log(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+console.log("payment intent key", process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(express.json());
 app.use(cors());
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vdxshj0.mongodb.net/?retryWrites=true&w=majority`;
-
+console.log("mongo db", process.env.DB_USER);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,6 +32,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const cartsCollection = client.db("bistroDb").collection("carts");
     const usersCollection = client.db("bistroDb").collection("users");
+    const paymentsCollection = client.db("bistroDb").collection("payments");
     // todo:verify jwt signature
     const verifyToken = (req, res, next) => {
       if (!req.headers.authorization) {
@@ -60,6 +65,66 @@ async function run() {
         expiresIn: "1h",
       });
       res.send({ token: token });
+    });
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      if (!price || amount < 1) return;
+      const parsedInt = parseInt(amount);
+      console.log(parsedInt, "amount inside payment intent");
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: parsedInt,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+    // payment collecting intent
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentsCollection.insertOne(payment);
+      console.log(payment);
+
+      // delete paid items from carts
+
+      const query = {
+        _id: {
+          $in: payment.cartId.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartsCollection.deleteMany(query);
+      res.send({ paymentResult: paymentResult, deleteResult: deleteResult });
+    });
+    // admin stats
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentsCollection.estimatedDocumentCount();
+
+      const result = await paymentsCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null, //to group all the elements
+              totalRevenue: { $sum: "$price" },
+            },
+          },
+        ])
+        .toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({ users, menuItems, orders, revenue });
+    });
+    // order-stats
+    app.get("/order-stats", async (req, res) => {
+      const result = await paymentsCollection
+        .aggregate([
+          {
+            $unwind: "$menuItemIds",
+          },
+        ])
+        .toArray();
+      res.send({ result});
     });
     // admin validation methods
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
@@ -139,6 +204,12 @@ async function run() {
       // console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
+    // post menu items
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
       res.send(result);
     });
     app.get("/menu", async (req, res) => {
